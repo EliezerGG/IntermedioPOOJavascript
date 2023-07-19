c7306b42b640b0cde666eb0e18b85d052fc5b1e1
Eliezer Guevara <magdiel.852@gmail.com>
2023-07-18 20:44:23 -0600

    defineproperty

---
+++ defineProperty.js
@@ -0,0 +1,36 @@
+
+Object.defineProperty(juan,"pruebaNasa",{
+    value: "Extraterrestres",
+    writable: false,
+    enumerable: false,
+    configurable: false,
+});
+
+Object.defineProperty(juan, "",{
+    value: "",
+    writable: true,
+    enumerable: true,
+    configurable:true,
+});
+
+Object.defineProperty(juan,"navigator",{
+    value: "Edge",
+    writable: true,
+    enumerable: false,
+    configurable: true,
+});
+
+Object.defineProperty(juan,"editor",{
+    value: "VS Code",
+    writable: false,
+    enumerable: true,
+    configurable: true,
+});
+
+
+Object.defineProperty(juan,"terminal",{
+    value: "WSL",
+    writable: true,
+    enumerable: true,
+    configurable: false,
+});
---
+++ main.js
@@ -0,0 +1,17 @@
+const juan = {
+    name: "Juanito",
+    age: 18,
+    approvedCourse: ["Curso1"],
+    addCourse(newCourse){
+        this.approvedCourse.push(newCourse);
+    }
+};
+
+console.log(Object.keys(juan));
+console.log(Object.getOwnPropertyNames(juan));
+console.log(Object.entries(juan));
+
+
+
+
+console.log(Object.getOwnPropertyDescriptors(juan));
